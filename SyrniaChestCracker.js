a53f80277b635ca7c68520c58a3c0712f8b5c4db
Makeshift <makeshift@itch.com>
2015-11-06 11:46:10 +0000

    Fixed the regex for array generation
    Turns out I was using PHP regex...

--- SyrniaChestCracker.js
+++ SyrniaChestCracker.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Syrnia Chest-Opener
 // @namespace    http://github.com/Makeshift
-// @version      0.1
+// @version      0.7
 // @description  Pick the size of the chest and refresh, will automatically enter and submit numbers for chests at random. Resets if you refresh the page!
 // @author       PatchworkTiger
 // @match        http://www.syrnia.com/game.php
@@ -9,7 +9,7 @@
 // ==/UserScript==
 //We require jquery because it's just easier to use jqueries selectors
 //Config
-var timeBetweenChecks = 100; //Lower can be laggier but faster checking.
+var timeBetweenChecks = 1000; //Lower can be laggier but faster checking.
 
 //Globals
 var alreadyRunning = false;
@@ -21,7 +21,7 @@ lookForInput();
 function lookForInput() {
     if (document.getElementById("guessCombo")) { //Check if it exists
         if (alreadyRunning == true) {
-            console.log("Trying: " + numArray[0] + "; Left: " + numArray.length()); //Debug console
+            console.log("Trying: " + numArray[0] + "; Left: " + numArray.size()); //Debug console
 
             document.getElementById("guessCombo").value = numArray[0]; //Set value
             $('input[type="submit"][value="Open"]').submit(); //Submit
@@ -34,18 +34,23 @@ function lookForInput() {
             setTimeout(lookForInput, timeBetweenChecks);
         } else {
             //Array builder
-            var text = document.getElementById("LocationContent");
-            var re = /between (\d+) and (\d+)./g;
-            var found = text.match(re); //Find th two numbers
-            console.log("Generating array between " + found[0] + " and " + found[1]);
-            for (i = found[0]; i <= found[1]; i++) { //Generate the array
-                numArray.push(i);
+            var str = document.getElementById("LocationContent").innerHTML;
+            var re = /between (.*) and (.*)\.\.\.<form/g;
+            var m;
+            while ((m = re.exec(str)) !== null) {
+                if (m.index === re.lastIndex) {
+                    re.lastIndex++;
+                }
+                console.log("Generating array between " + m[1] + " and " + m[2]);
+                for (i = m[1]; i <= m[2]; i++) { //Generate the array
+                    numArray.push(i);
+                }
             }
             alreadyRunning = true;
             setTimeout(lookForInput, timeBetweenChecks);
             //Note: If the use refreshes, we will lose progress
         }
     } else {
-      setTimeout(lookForInput, timeBetweenChecks);
+        setTimeout(lookForInput, timeBetweenChecks);
     }
 }
